95ccaa5303f99b36570c83a9e13670bb23c6386d
yosuke ota <otameshiyo23@gmail.com>
2023-02-10 08:39:46 +0900

    fix

--- tests/fixtures/standard/stylelint.config.js
+++ tests/fixtures/standard/stylelint.config.js
@@ -2,4 +2,9 @@
 
 module.exports = {
     extends: ["stylelint-config-standard", "stylelint-stylus/standard"],
+    rules: {
+        "no-missing-end-of-source-newline": true,
+        'string-quotes': 'double',
+		'declaration-colon-space-after': 'always-single-line',
+    }
 }
