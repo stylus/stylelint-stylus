1f9961f7b0aac9b6f4665b820567b64019f6eb3f
yosuke ota <otameshiyo23@gmail.com>
2023-02-10 08:57:27 +0900

    fix

--- tests/fixtures/standard/stylelint.config.js
+++ tests/fixtures/standard/stylelint.config.js
@@ -1,10 +1,12 @@
-"use strict"
+"use strict";
 
 module.exports = {
-    extends: ["stylelint-config-standard", "stylelint-stylus/standard"],
-    rules: {
-        "no-missing-end-of-source-newline": true,
-        'string-quotes': 'double',
-		'declaration-colon-space-after': 'always-single-line',
-    }
-}
+  extends: ["stylelint-config-standard", "stylelint-stylus/standard"],
+  rules: {
+    "no-missing-end-of-source-newline": true,
+    "string-quotes": "double",
+    "declaration-colon-space-after": "always-single-line",
+    "declaration-colon-space-before": "never",
+    "declaration-block-semicolon-newline-after": "always-multi-line",
+  },
+};
